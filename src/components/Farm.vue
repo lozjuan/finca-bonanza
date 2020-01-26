365bdd547fde78bdec3f3d3b598076b5f56b5146
lozjuan <lozjuan@gmail.com>
2020-01-26 21:25:17 +0100

    text

--- src/components/Farm.vue
+++ src/components/Farm.vue
@@ -18,28 +18,49 @@
 				<div id="tabs-content">
 					<ul>
 						<li>
-							<img v-if="hover === 1" id="icons" src="../assets/about-us.svg">
-							<img v-if="hover === 1" id="text" src="../assets/about.svg">								 
-							<img v-if="hover === 2" id="icons" src="../assets/pin.svg">
-							<img v-if="hover === 2" id="text" src="../assets/loc.svg">
-							<img v-if="hover > 2" id="icons" src="../assets/cow.svg">
-							<img v-if="hover > 2" id="text" src="../assets/cattle.svg">
+							<img v-if="hover === 1" src="../assets/about-us.svg">
+							<p v-if="hover === 1" id="us-text">
+								We are a young family project, passionate about coffee and nature. 
+								We work for high quality coffee and environmental preservation. 
+							</p>
+							<img v-if="hover === 2" src="../assets/pin.svg">
+							<p v-if="hover === 2" id="us-text">
+								The farm is located in the municipality of Coper Boyaca 60 miles north of Bogota, Colombia.
+							</p>
+							<img v-if="hover > 2" src="../assets/cow.svg">
+							<p v-if="hover > 2" id="us-text">
+								We are replacing livestock land with coffee crops, reducing methane emissions and creating employment. We are an animal-friendly project.
+							</p>
 						</li>
 						<li>
-							<img v-if="hover === 1" id="icons" src="../assets/home-delivery-of-goods.svg">
-							<img v-if="hover === 1" id="text" src="../assets/consumers.svg">
-							<img v-if="hover === 2" id="icons" src="../assets/mountain.svg">
-							<img v-if="hover === 2" id="text" src="../assets/altitude.svg">
-							<img v-if="hover > 2" id="icons" src="../assets/water-sources.svg">
-							<img v-if="hover > 2" id="text" src="../assets/water-source.svg">
+							<img v-if="hover === 1" src="../assets/home-delivery-of-goods.svg">
+							<p v-if="hover === 1" id="us-text">
+								We work to get coffee beans from our farm directly to consumers. We would love to send our coffee to your restaurant, bar or company. Our clients are very welcome to our farm.   
+							</p>
+							<img v-if="hover === 2" src="../assets/mountain.svg">
+							<p v-if="hover === 2" id="us-text">
+								We are on the eastern mountain range of Colombia at an altitude over 6000 ft above sea level.
+							</p>
+							<img v-if="hover > 2" src="../assets/water-sources.svg">
+							<p v-if="hover > 2" id="us-text">
+								We take special care of our water resources. All the water used in the coffee process is treated and evaporated in retention containers.
+							</p>
 						</li>
 						<li>
-							<img v-if="hover === 1" id="icons" src="../assets/044-connect.svg">
-							<img v-if="hover === 1" id="text" src="../assets/workers.svg">
-							<img v-if="hover === 2" id="icons" src="../assets/weather.svg">
-							<img v-if="hover === 2" id="text" src="../assets/weather-2.svg">
-							<img v-if="hover > 2" id="icons" src="../assets/forest.svg">
-							<img v-if="hover > 2" id="text" src="../assets/forest-2.svg"> 								
+							<img v-if="hover === 1" src="../assets/044-connect.svg">
+							<p v-if="hover === 1" id="us-text">
+								Thanks to the project many families in the region have been benefited. 
+								Job creation in decent conditions is our pride.
+							</p>
+							<img v-if="hover === 2" src="../assets/weather.svg">
+							<p v-if="hover === 2" id="us-text">
+								The climate and altitude are perfect for cultivation. The rain is enough for irrigation and cloudiness gives the necessary shade for plants.
+							</p>
+							<img v-if="hover > 2" src="../assets/forest.svg">
+							<p v-if="hover > 2" id="us-text"> 
+								We protect a large area of native forest. all our water resources are born in the forest.
+								Therefore, the protection of nature is our priority.
+							</p>
 						</li>
 					</ul>
 				</div>
@@ -98,9 +119,6 @@ export default {
 		padding-left: 5rem;
 		margin-top: -1rem;
     	width: 255%;
-	}
-	#farm-title img {
-	    margin-top: 1rem;
 	}
 	#farm-photos {
 		width: 230rem;
